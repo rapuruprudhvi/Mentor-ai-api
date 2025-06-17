03a7ed2ca61ccc10abb0698ab15a9afafa3e27e7
prudhvirapuru <prudhvirapuru@gmail.com>
2025-06-17 17:44:27 +0530

    fix: handler and router

--- src/router/open.ai.route.ts
+++ src/router/open.ai.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { CreateInterviewPromptHandler } from "../handler/create.open.ai.handler";
-import { resolveRouteHandler } from "../utils/handler.resolver";
+import { resolveRouteHandler } from "../utils/handler.utils";
 
 const interviewPromptsRouter = Router({ mergeParams: true });
 
