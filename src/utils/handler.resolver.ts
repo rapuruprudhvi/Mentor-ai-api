366937a22321456eff6ea5a88610e103760653b8
prudhvirapuru <prudhvirapuru@gmail.com>
2025-06-17 17:39:32 +0530

    fix: handle OpenAI stream error in handler.resolver

--- src/utils/handler.resolver.ts
+++ src/utils/handler.resolver.ts
@@ -1,11 +1,3 @@
-// // src/util/handler.resolver.ts
-// export const resolveRouteHandler = (HandlerClass: any) => {
-//   const instance = new HandlerClass();
-//   return (req: any, res: any) => instance.handle(req, res);
-// };
-
-
-// src/util/handler.resolver.ts
 import { InterviewPromptService } from "../service/interview.prompt.service";
 import { CreateInterviewPromptHandler } from "../handler/create.open.ai.handler";
 
