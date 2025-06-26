22644b135b3f2ae70b0e75f2ad5bbdb192ed954d
prudhvirapuru <prudhvirapuru@gmail.com>
2025-06-26 12:18:39 +0530

    create and get the each interview individually based on the userId and sesionId

--- src/router/interview.router.ts
+++ src/router/interview.router.ts
@@ -4,12 +4,17 @@ import { StartInterviewHandler } from "../handler/start-interview.handler";
 import { EndInterviewHandler } from "../handler/end-interview.handler";
 import { resolveRouteHandler } from "../utils/handler.utils";
 import { PassportAuthMiddleware } from '../middleware/auth.middleware';
+import { AskInterviewQuestionHandler } from "../handler/interview.sesion.handler";
+import { GetInterviewQuestionsHandler } from "../handler/get-interview-questions.handler";
 
 const interviewRouter = Router({ mergeParams: true });
 
 
 interviewRouter.post("/start", resolveRouteHandler(StartInterviewHandler));
 interviewRouter.post("/:sessionId/end", resolveRouteHandler(EndInterviewHandler));
+interviewRouter.post("/:sessionId", resolveRouteHandler(AskInterviewQuestionHandler));
+interviewRouter.get("/:sessionId", resolveRouteHandler(GetInterviewQuestionsHandler));
+
 
 const interviewsRouter = Router({ mergeParams: true });
 
