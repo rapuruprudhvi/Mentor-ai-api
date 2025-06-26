67f49ea9466d16fd32c42c9021d8ca16c43fc33e
prudhvirapuru <prudhvirapuru@gmail.com>
2025-06-26 19:18:10 +0530

    Displaying the interview titles

--- src/router/interview.router.ts
+++ src/router/interview.router.ts
@@ -1,32 +1,3 @@
-// // interview.route.ts
-// import { Router } from "express";
-// import { StartInterviewHandler } from "../handler/start-interview.handler";
-// import { EndInterviewHandler } from "../handler/end-interview.handler";
-// import { resolveRouteHandler } from "../utils/handler.utils";
-// import { PassportAuthMiddleware } from '../middleware/auth.middleware';
-// import { AskInterviewQuestionHandler } from "../handler/interview.sesion.handler";
-// import { GetInterviewQuestionsHandler } from "../handler/get-interview-questions.handler";
-// import { GetUserInterviewTitlesHandler } from "../handler/get.interview.title.handler";
-
-// const interviewRouter = Router({ mergeParams: true });
-
-
-// interviewRouter.post("/start", resolveRouteHandler(StartInterviewHandler));
-// interviewRouter.post("/:sessionId/end", resolveRouteHandler(EndInterviewHandler));
-// interviewRouter.post("/:sessionId", resolveRouteHandler(AskInterviewQuestionHandler));
-// interviewRouter.get("/:sessionId", resolveRouteHandler(GetInterviewQuestionsHandler));
-// interviewRouter.get("/title", resolveRouteHandler(GetUserInterviewTitlesHandler ));
-
-
-
-// const interviewsRouter = Router({ mergeParams: true });
-
-// interviewsRouter.use("", resolveRouteHandler(PassportAuthMiddleware), interviewRouter);
-
-// export default interviewsRouter;
-
-
-// interview.route.ts
 import { Router } from "express";
 import { StartInterviewHandler } from "../handler/start-interview.handler";
 import { EndInterviewHandler } from "../handler/end-interview.handler";
@@ -38,11 +9,9 @@ import { GetUserInterviewTitlesHandler } from "../handler/get.interview.title.ha
 
 const interviewRouter = Router({ mergeParams: true });
 
-// ✅ Move specific routes BEFORE parameterized routes
 interviewRouter.get("/title", resolveRouteHandler(GetUserInterviewTitlesHandler));
 interviewRouter.post("/start", resolveRouteHandler(StartInterviewHandler));
 
-// ✅ Parameterized routes should come after specific routes
 interviewRouter.post("/:sessionId/end", resolveRouteHandler(EndInterviewHandler));
 interviewRouter.post("/:sessionId", resolveRouteHandler(AskInterviewQuestionHandler));
 interviewRouter.get("/:sessionId", resolveRouteHandler(GetInterviewQuestionsHandler));
--- src/router/open.ai.router.ts
+++ src/router/open.ai.router.ts
@@ -1,20 +1,3 @@
-// import { Router } from "express";
-// import { CreateInterviewPromptHandler } from "../handler/create.open.ai.handler";
-// import { resolveRouteHandler } from "../utils/handler.utils";
-// import { GetAllPromptsHandler } from "../handler/get.all.prompts.handler";
-
-// const interviewPromptsRouter = Router({ mergeParams: true });
-
-// interviewPromptsRouter.get("/", resolveRouteHandler(GetAllPromptsHandler));
-
-// interviewPromptsRouter.post(
-//   "",
-//   resolveRouteHandler(CreateInterviewPromptHandler)
-// );
-// export default interviewPromptsRouter;
-
-
-
 import { Router } from "express";
 import { CreateInterviewPromptHandler } from "../handler/create.open.ai.handler";
 import { GetAllPromptsHandler } from "../handler/get.all.prompts.handler";
